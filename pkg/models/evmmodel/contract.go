e9eb605ef5b9988cabd7b0c9ea4a744ea48aa1d0
imxyb <xyb4638@gmail.com>
2022-11-07 17:39:45 +0800

    fix:contract field

--- pkg/models/evmmodel/contract.go
+++ pkg/models/evmmodel/contract.go
@@ -5,7 +5,7 @@ type Contract struct {
 	Height          int64  `xorm:"bigint notnull pk"`
 	Version         int    `xorm:"integer notnull pk"`
 	Address         string `xorm:"varchar(255) notnull pk"`
-	FilecoinAddress string `xorm:"varchar(255) notnull" default ''`
+	FilecoinAddress string `xorm:"varchar(255) notnull default ''"`
 	Balance         int64  `xorm:"bigint notnull default 0"`
 	Nonce           uint64 `xorm:"bigint notnull default 0"`
 	ByteCode        string `xorm:"text notnull default ''"`
