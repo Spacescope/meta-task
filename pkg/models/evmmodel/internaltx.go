a3ab9c4e5944dc8b93a1e1c8ec92d629285ef303
cxytz01 <bluecxy@outlook.com>
2023-10-09 11:28:16 +0800

    evm_internal_tx replenish the parent_message_cid field

--- pkg/models/evmmodel/internaltx.go
+++ pkg/models/evmmodel/internaltx.go
@@ -4,16 +4,17 @@ import "time"
 
 // InternalTX contract internal transaction
 type InternalTX struct {
-	Height      int64     `xorm:"bigint notnull index"`
-	Version     int       `xorm:"integer notnull"`
-	ParentHash  string    `xorm:"varchar(255) notnull index default ''"`
-	Type        uint64    `xorm:"bigint notnull default 0"`
-	From        string    `xorm:"varchar(255) notnull index default ''"`
-	To          string    `xorm:"varchar(255) notnull index default ''"`
-	Value       string    `xorm:"varchar(100) notnull default '0'"`
-	Params      string    `xorm:"text notnull default ''"`
-	ParamsCodec uint64    `xorm:"bigint notnull default 0"`
-	CreatedAt   time.Time `xorm:"created"`
+	Height           int64     `xorm:"bigint notnull index"`
+	Version          int       `xorm:"integer notnull"`
+	ParentHash       string    `xorm:"varchar(255) notnull index default ''"`
+	ParentMessageCid string    `xorm:"varchar(255) notnull index default ''"`
+	Type             uint64    `xorm:"bigint notnull default 0"`
+	From             string    `xorm:"varchar(255) notnull index default ''"`
+	To               string    `xorm:"varchar(255) notnull index default ''"`
+	Value            string    `xorm:"varchar(100) notnull default '0'"`
+	Params           string    `xorm:"text notnull default ''"`
+	ParamsCodec      uint64    `xorm:"bigint notnull default 0"`
+	CreatedAt        time.Time `xorm:"created"`
 }
 
 func (i *InternalTX) TableName() string {
